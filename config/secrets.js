2cdd929e7ee6924387e0f96a84c080d111de6604
Heinrich Fenkart <hnrch02@gmail.com>
2014-06-02 23:40:10 +0200

    Remove support for MONGOHQ_URL env variable

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: process.env.MONGODB || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gamekeller',
+  db: process.env.MONGODB || 'mongodb://localhost:27017/gamekeller',
   sessionSecret: process.env.SESSION_SECRET || 'gkdevel'
 }
