9f30c11fbbb56a910693ac12c048dd06bf615736
Heinrich Fenkart <hnrch02@gmail.com>
2016-08-24 18:22:55 +0200

    User/friends: deleted users are now removed from the friends list
    Fixes #79.

---
+++ migrations/1472043322007-deleted-friends.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var _ = require('lodash')
+var Promise = require('bluebird')
+var mongoose = require('mongoose')
+var config = require('../lib/config')
+var User = require('../lib/models/User')
+var connection = require('../lib/mongo')
+
+exports.up = function(next) {
+  var usersById
+
+  connection
+  .then(function() {
+    return User.find({}, '_id', { lean: true })
+  })
+  .then(function(existingUsers) {
+    usersById = _.map(existingUsers, '_id')
+
+    return User.find({ 'friends.0': { $exists: true } }, 'friends', { lean: true })
+  })
+  .each(function(user) {
+    console.log('processing user', user._id)
+
+    user.friends = _.intersectionWith(user.friends, usersById, function(val, other) {
+      return val.equals(other)
+    })
+
+    return User.update({ _id: user._id }, { $set: { friends: user.friends } })
+  })
+  .then(function() {
+    next()
+  })
+  .catch(function(err) {
+    next(err)
+  })
+}
+
+exports.down = function(next) {
+  next()
+}
